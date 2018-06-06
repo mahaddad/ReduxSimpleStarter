314d30c48c817d2c11a1f3e8f4ba531187a43753
mahaddad <michael@carriage.io>
2018-06-05 23:17:28 -0400

    start of implementation of a container class

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+//  Step 1 - Create reducer
+//  Step 2 - Wire up to the application
+
+export default function() {
+    return [
+        {title: 'JavaScript: The Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'},
+    ]
+}
